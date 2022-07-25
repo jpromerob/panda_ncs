6d3f475822fb13c329852f1ffadfcb371f65fbb6
Juan Pablo Romero Bermudez <jprb@kth.se>
2022-07-25 14:54:52 +0200

    Simple 'Planner'

---
+++ capsense.cpp
@@ -0,0 +1,586 @@
+
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <inttypes.h>
+#include <stdio.h>
+#include <termios.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <mutex>
+#include <cstdio>
+#include <algorithm>
+#include <stdlib.h>
+#include <thread>    
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+
+
+
+
+#include "ncs_common.h"
+
+#include <Eigen/Dense>
+
+#include <franka/duration.h>
+#include <franka/exception.h>
+#include <franka/model.h>
+#include <franka/robot.h>
+#include <franka/gripper.h>
+#include <franka/robot.h>
+#include <franka/spline.h>
+
+#include <franka/NatNetTypes.h>
+#include <franka/NatNetCAPI.h>
+#include <franka/NatNetClient.h>
+
+using namespace std;
+
+
+#pragma pack(1)
+
+typedef struct xyz_payload_t {
+    float x;
+    float y;
+    float z;
+} xyz_payload;
+
+typedef struct joints_payload_t {
+    double q0;
+    double q1;
+    double q2;
+    double q3;
+    double q4;
+    double q5;
+    double q6;
+} joints_payload;
+
+#pragma pack()
+
+
+int op_mode;
+
+
+double scale = 0.9; // scaling constant 
+double max_v = 1.7 * scale; // velocity
+double max_a = 13.0 * scale; // acceleration
+// double max_j = 6500.0 * scale; // Jerk
+double max_j = 100; // Jerk
+
+typedef struct coor
+{
+    double x;
+    double y;
+    double z;
+
+} coor;
+
+std::mutex mutex_sensors;
+std::mutex mutex_nextcoor;
+std::mutex mutex_p_current;
+
+
+/* Limits of the workspace*/
+coor minimum;
+coor maximum;
+
+/* Current Target */
+coor c_target;
+coor u_target;
+
+/* What the optitrack sees (in Panda coordinate framework)*/
+coor nextcoor;
+
+/* Proprioception */
+coor p_current;
+coor p_delta;
+
+
+
+/***********************************************************************************************/
+/* This function returns a 'final_value' for 'value' limited by the accesible workspace        */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+double check_limit(double value, char axis) {
+
+    double final_value = value;
+    double max_val = 0.15;
+    double min_val = 0.15;
+    
+    if(axis == 'x') {
+      max_val = maximum.x;
+      min_val = minimum.x;
+    }
+    if(axis == 'y') {
+      max_val = maximum.y;
+      min_val = minimum.y;
+    }
+    if(axis == 'z') {
+      max_val = maximum.z;
+      min_val = minimum.z;
+    }
+
+    // Limit Max Val for 'axis'
+    if (final_value > max_val) {
+      final_value = max_val;
+    }
+
+    // Limit Min Val for 'axis'
+    if (final_value < min_val) {
+      final_value = min_val;
+    }
+
+    // std::cout << "Target " << axis << ": " << final_value << std::endl;
+
+    return final_value;
+}
+
+
+
+/***********************************************************************************************/
+/* x, y, z are converted from optitrack coordinates to panda coordinates and stored in 'nextcoor'
+/*
+/*
+/*
+/*
+/***********************************************************************************************/
+void save_nextcoor(double x, double y, double z) {
+
+  double offset_x = 0.0; // offset from object origin to robot end effector
+  double offset_y = 0.0;
+  double offset_z = 0.0;
+
+  if (mutex_nextcoor.try_lock()) {
+    nextcoor.x = check_limit( x + 0.35 + offset_x, 'x'); 
+    nextcoor.y = check_limit(-z + 0.36 + offset_y, 'y');
+    nextcoor.z = check_limit( y + 0.01 + offset_z, 'z');
+    mutex_nextcoor.unlock();
+  }
+
+
+}
+
+int t_counter = 0;
+double init_t = 0.0;
+
+
+/***********************************************************************************************/
+/* This functions just creates a socket for TCP communication                                  */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+
+int createSocket(int port)
+{
+    int sock, err;
+    struct sockaddr_in server;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("ERROR: Socket creation failed\n");
+        exit(1);
+    }
+
+    bzero((char *) &server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+    if (bind(sock, (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        printf("ERROR: Bind failed\n");
+        exit(1);
+    }
+    printf("Listening to Sleipner\n");
+
+    listen(sock , 3);
+
+    return sock;
+}
+
+
+
+void closeSocket(int sock)
+{
+    close(sock);
+    return;
+}
+
+void sendMsg(int sock, void* msg, uint32_t msgsize)
+{
+    if (write(sock, msg, msgsize) < 0)
+    {
+        printf("Can't send message.\n");
+        closeSocket(sock);
+        exit(1);
+    }
+    printf("Message sent (%d bytes).\n", msgsize);
+    return;
+}
+
+/***********************************************************************************************/
+/* This function is in charge of receiving data (x, y, z predicted by SNN) through tcp socket  */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+int get_ws_coordinates()
+{
+    int PORT = 2600;
+    int BUFFSIZE = 512;
+    char buff[BUFFSIZE];
+    int ssock, csock;
+    int nread;
+    struct sockaddr_in client;
+    socklen_t clilen = sizeof(client);
+
+    ssock = createSocket(PORT);
+    printf("Server listening on port %d\n", PORT);
+
+    while (1)
+    {
+        csock = accept(ssock, (struct sockaddr *)&client, &clilen);
+        if (csock < 0)
+        {
+            printf("Error: accept() failed\n");
+            continue;
+        }
+
+        bzero(buff, BUFFSIZE);
+        while ((nread=read(csock, buff, BUFFSIZE)) > 0)
+        {
+            xyz_payload *p = (xyz_payload*) buff;
+
+            save_nextcoor(p->x, p->y, p->z);
+            printf("x: %3.3f | y: %3.3f | z: %3.3f\n", p->x, p->y, p->z);
+              
+        }
+        close(csock);
+    }
+
+    close(ssock);
+    return 0;
+}
+
+/***********************************************************************************************/
+/* Max and min values in robot coordinate frame                                                */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+void init_maxmin_values(){
+
+
+  minimum.x = -0.20;
+  minimum.y = -0.80;
+  minimum.z = 0.10;
+
+  maximum.x = 0.60; //
+  maximum.y = 0.20; // +y points to munin
+  maximum.z = 0.80; // up down
+
+}
+
+
+
+/***********************************************************************************************/
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+void read_csv()
+{
+  vector<string> row;
+  ifstream fin;
+  string line, word;
+  // Open an existing file
+  fin.open("examples/FakeOptiTrack.csv");
+  while(!fin.eof()){
+    getline(fin, line);
+    stringstream s(line);
+    while (getline (s, word, ',')) {
+        row.push_back (word);
+    }
+    cout << " x: " << row[0] << " y: " << row[1] << " z: " << row[2] << "\n";
+  }
+}
+
+
+double validate(double curr_coor, double next_coor){
+
+    double delta = 0.10; // 10[cm]
+    double valid_coor = next_coor;
+    if(next_coor > curr_coor + delta){
+      valid_coor = curr_coor + delta;
+    }
+    if(next_coor < curr_coor - delta){
+      valid_coor = curr_coor - delta;
+    }
+
+    return valid_coor;
+
+}
+
+/***********************************************************************************************/
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+void close_gripper(char * robot_ip) {
+
+    franka::Robot robot(robot_ip);
+    franka::Gripper gripper(robot_ip);
+    gripper.homing();
+    
+
+    // Grasp the object: speed + force
+    double object_width = 3/1000.0; // [m]
+    double speed = 0.2; // [m/s]
+    double force = 140; // [N]
+
+    std::cout << "Grasping ... " << std::endl;
+    
+    if (!gripper.grasp(object_width, speed, force)) {
+      std::cout << "Failed to grasp object." << std::endl;
+    }
+
+}
+
+
+/* A new target get approved if it's different from current position by at >2mm in at least one axis */
+bool target_approval(double x_i, double y_i, double z_i, double x_f, double y_f, double z_f) {
+
+  bool flag = false;
+  double delta = 0.002;
+
+  if((abs(x_i-x_f) > delta) || (abs(y_i-y_f) > delta) || (abs(z_i-z_f) > delta)) {
+    flag = true;
+  }
+
+
+  return flag;
+}
+
+void init_panda_pva(char* robot_ip) {
+
+  franka::Robot robot(robot_ip);
+  robot.read([](const franka::RobotState& robot_state) {
+
+    std::array<double, 16> final_pose;
+    std::array<double, 7> final_joints;
+    final_pose = robot_state.O_T_EE_c;
+    final_joints = robot_state.q_d;
+
+
+    if (mutex_nextcoor.try_lock()) {
+      nextcoor.x = final_pose[12]; 
+      nextcoor.y = final_pose[13];
+      nextcoor.z = final_pose[14];
+      mutex_nextcoor.unlock();
+    }
+
+    return false;
+  });
+
+}
+
+
+#define PI 3.14159265
+
+std::array<double, 7> rob_joints;
+void get_sensors() {
+
+  std::array<double, 7> rj;
+  while(1){
+
+    if (mutex_sensors.try_lock()) {
+      rj = rob_joints;
+      mutex_sensors.unlock();
+    }
+    printf("q0: %3.3f ", rj[0]);
+    printf("q1: %3.3f ", rj[1]);
+    printf("q2: %3.3f ", rj[2]);
+    printf("q3: %3.3f ", rj[3]);
+    printf("q4: %3.3f ", rj[4]);
+    printf("q5: %3.3f ", rj[5]);
+    printf("q6: %3.3f ", rj[6]);
+    printf("\n");
+    usleep(100000);
+
+  }
+}
+
+void move_end_effector(char* robot_ip) {
+
+  std::cout << "Are things ready ???" << std::endl;
+  std::cin.ignore();
+  
+  std::cout << robot_ip << std::endl;  
+
+  // Compliance parameters
+  const double translational_stiffness{150.0};
+  const double rotational_stiffness{10.0};
+  Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
+  stiffness.setZero();
+  stiffness.topLeftCorner(3, 3) << translational_stiffness * Eigen::MatrixXd::Identity(3, 3);
+  stiffness.bottomRightCorner(3, 3) << rotational_stiffness * Eigen::MatrixXd::Identity(3, 3);
+  damping.setZero();
+  damping.topLeftCorner(3, 3) << 2.0 * sqrt(translational_stiffness) *
+                                     Eigen::MatrixXd::Identity(3, 3);
+  damping.bottomRightCorner(3, 3) << 2.0 * sqrt(rotational_stiffness) *
+                                         Eigen::MatrixXd::Identity(3, 3);
+
+
+  try {
+
+    // connect to robot
+    franka::Robot robot(robot_ip);
+    setDefaultBehavior(robot);
+    // load the kinematics and dynamics model
+    franka::Model model = robot.loadModel();   
+    franka::RobotState initial_state = robot.readOnce();
+
+
+
+    // set collision behavior
+    robot.setCollisionBehavior({{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}});
+
+    // define callback for the torque control loop
+    std::function<franka::Torques(const franka::RobotState&, franka::Duration)>
+        impedance_control_callback = [&](const franka::RobotState& robot_state,
+                                         franka::Duration /*duration*/) -> franka::Torques {
+
+                            
+      mutex_nextcoor.lock();
+      c_target.x = validate(robot_state.O_T_EE[12], nextcoor.x);
+      c_target.y = validate(robot_state.O_T_EE[13], nextcoor.y);
+      c_target.z = validate(robot_state.O_T_EE[14], nextcoor.z); 
+      mutex_nextcoor.unlock();           
+  
+      mutex_sensors.lock();      
+      rob_joints = robot_state.q;
+      mutex_sensors.unlock();        
+                 
+
+      
+
+      // actual end effector positioning command
+      initial_state.O_T_EE[12] = c_target.x;
+      initial_state.O_T_EE[13] = c_target.y;
+      initial_state.O_T_EE[14] = c_target.z;
+
+
+      // equilibrium point is the initial position
+      Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+      Eigen::Vector3d position_d(initial_transform.translation());
+      Eigen::Quaterniond orientation_d(initial_transform.linear());
+
+      // get state variables
+      std::array<double, 7> coriolis_array = model.coriolis(robot_state);
+      std::array<double, 42> jacobian_array =
+          model.zeroJacobian(franka::Frame::kEndEffector, robot_state);
+
+      // convert to Eigen
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
+      Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
+      Eigen::Vector3d position(transform.translation());
+      Eigen::Quaterniond orientation(transform.linear());
+
+
+      // compute error to desired equilibrium pose
+      // position error
+      Eigen::Matrix<double, 6, 1> error;
+      error.head(3) << 1.5*(position - position_d);
+
+      // orientation error
+      // "difference" quaternion
+      if (orientation_d.coeffs().dot(orientation.coeffs()) < 0.0) {
+        orientation.coeffs() << -orientation.coeffs();
+      }
+      // "difference" quaternion
+      Eigen::Quaterniond error_quaternion(orientation.inverse() * orientation_d);
+      error.tail(3) << error_quaternion.x(), error_quaternion.y(), error_quaternion.z();
+      // Transform to base frame
+      error.tail(3) << -transform.linear() * error.tail(3);
+
+      // compute control
+      Eigen::VectorXd tau_task(7), tau_d(7);
+
+      // Spring damper system with damping ratio=1
+      tau_task << jacobian.transpose() * (-stiffness * error - damping * (jacobian * dq));
+      tau_d << tau_task + coriolis;
+
+      std::array<double, 7> tau_d_array{};
+      Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
+      return tau_d_array;
+    };
+
+    robot.control(impedance_control_callback);
+
+
+  } catch (const franka::Exception& ex) {
+    // print exception
+    std::cout << ex.what() << std::endl;
+  }
+
+}
+
+
+/***********************************************************************************************/
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/*                                                                                             */
+/***********************************************************************************************/
+
+
+int main(int argc, char** argv) {
+
+  op_mode = 0; // 0: only reading SNN data, 1: closed loop SNN, 2: closed loop optitrack
+
+  std::cout << "JPRB: Hello NCS people :)\n";
+  
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <robot-ip>" << std::endl;
+    return -1;
+  }
+  init_maxmin_values();
+
+  // When the program starts the robot 'moves' to its current position
+  // This is to prevent sudden motion
+  init_panda_pva(argv[1]);
+
+  close_gripper(argv[1]);
+   
+
+  std::thread coor_process (get_ws_coordinates);
+  std::thread mot_process (move_end_effector, argv[1]);  
+  coor_process.join(); 
+  printf("Stopped getting incoming data\n");
+  mot_process.join();
+
+  return 0;
+}
---
+++ middler/planner.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+""" client.py - Echo client for sending/receiving C-like structs via socket
+
+References:
+- Ctypes: https://docs.python.org/3/library/ctypes.html
+- Sockets: https://docs.python.org/3/library/socket.html
+"""
+
+import socket
+import sys
+import math
+import random
+import time
+import multiprocessing
+from ctypes import *
+
+
+""" This class defines a C-like struct """
+class Coordinates(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("z", c_float)]
+
+
+class Joints(Structure):
+    _fields_ = [("q0", c_double),
+                ("q1", c_double),
+                ("q2", c_double),
+                ("q3", c_double),
+                ("q4", c_double),
+                ("q5", c_double),
+                ("q6", c_double)]
+
+
+def generate_coordinates():
+    server_addr = ('172.16.222.31', 2600)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect(server_addr)
+        print("Connected to {:s}".format(repr(server_addr)))
+
+        print("")
+
+        idx = 0
+        max_radius = 0.10
+        while(True):
+            
+
+            # Create a trajectory whose projections on axes x, y, and z are circles
+            delta_x = max_radius*math.sin(math.pi/180*0.5*idx)
+            delta_y = max_radius*math.sin(math.pi/180*2*idx)
+            delta_z = max_radius*math.sin(math.pi/180*idx)
+
+
+            panda_x = 0.000 + delta_x
+            panda_y = -0.500 + delta_y 
+            panda_z = 0.400 + delta_z
+
+            x = panda_x - 0.35
+            y = panda_z - 0.01
+            z = -panda_y + 0.36
+
+            xyz_out = Coordinates(x, y, z)
+
+            print("Sending x={:f}, y={:f}, z={:f}".format(xyz_out.x, xyz_out.y, xyz_out.z))
+            nsent = s.send(xyz_out)          
+            time.sleep(0.1)
+
+            idx += 2
+
+    except AttributeError as ae:
+        print("Error creating the socket: {}".format(ae))
+    except socket.error as se:
+        print("Exception on socket: {}".format(se))
+    finally:
+        print("Closing socket")
+        s.close()
+
+
+
+
+if __name__ == "__main__":
+    
+    coor_process = multiprocessing.Process(target=generate_coordinates, args=())
+
+    coor_process.start()   
+    coor_process.join()
