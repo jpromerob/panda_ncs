d17d67d0ecf2a1df10e97ff67add09f9a27c5c19
Juan-Pablo Romero-Bermudez <jprb@kth.se>
2023-10-24 20:07:23 +0200

    Adding visualizer (from NUC)

---
+++ visualizers/full_show.py
@@ -0,0 +1,185 @@
+
+import aestream
+import time
+import cv2
+import pdb
+import numpy as np
+import math
+import argparse
+import csv
+import os
+import multiprocessing
+import socket
+
+IP_SLEIPNER = "172.16.222.28"
+IP_NUC = "172.16.222.46"
+
+class cam_info:
+    def __init__(self, id, bus,  nbr,  p_sleipner, p_nuc):
+        self.id = id
+        self.tag = f"inivation {bus} {nbr} dvx"
+        self.p_sleipner = p_sleipner
+        self.p_nuc = p_nuc
+
+# Function for the processes
+def start_streaming(cam_info, mode, undistortion):    
+
+
+    if mode == "recording":
+        cmd_in = f"input file cam{cam_info.id}.aedat4"
+    elif mode == "live":
+        cmd_in = f"input {cam_info.tag}"
+    else:
+        print("wrong mode")
+        quit()
+
+    cmd_out = f"output udp {IP_SLEIPNER} {cam_info.p_sleipner} {IP_NUC} {cam_info.p_nuc}"
+
+    if undistortion:
+        cmd_und = f"resolution 640 480 undistortion cam{cam_info.id}.csv"
+        u_message = "WITH undistortion"
+    else:
+        cmd_und = ""
+        u_message = "withOUT undistortion"
+    cmd_full = f"/opt/aestream/build/src/aestream {cmd_in} {cmd_out} {cmd_und}"
+
+
+    time.sleep(2*cam_info.id)
+    print(f"Camera #{cam_info.id} ready to stream {u_message}...")
+    print(f"stream {cmd_in} {cmd_out} {cmd_und}")
+    os.system(cmd_full)
+
+def visualize_data(args):
+
+    window_name = 'Cobotics Workspace'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, (math.ceil(640*2*args.scale), math.ceil(480*2*args.scale)))
+
+
+
+    # Stream events from UDP port 3333 (default)
+    mgh = 40
+    mgi = 20
+    mgv = 40
+    radius = 10
+    frame = np.zeros((640*2+mgh*2+mgi,480*2+mgv*2+mgi,3))
+    marker = np.zeros((2*radius+1, 2*radius+1, 3))
+
+
+    red = (0, 0, 255) 
+    with aestream.UDPInput((640, 480), device = 'cpu', port=args.port1) as stream1:
+        with aestream.UDPInput((640, 480), device = 'cpu', port=args.port2) as stream2:
+            with aestream.UDPInput((640, 480), device = 'cpu', port=args.port3) as stream3:
+                
+                while True:
+
+                    frame[mgh+0:mgh+640,mgv+mgi+480:mgv+mgi+480*2,0:2] = stream1.read()[:,:,np.newaxis] # cyan
+                    frame[mgh+0:mgh+640,mgv+0:mgv+480,1] =  stream2.read() # green
+                    frame[mgh+mgi+640:mgh+mgi+640*2,mgv+0:mgv+480,1:3] = stream3.read()[:,:,np.newaxis] # yellow
+
+                    
+
+                    image = cv2.resize(frame.transpose(1,0,2), (math.ceil((640*2+mgh*2+mgi)*args.scale),math.ceil((480*2+mgv*2+mgi)*args.scale)), interpolation = cv2.INTER_AREA)
+                    
+                    cx = int((mgh+xy_array[0])*args.scale)
+                    cy = int((mgv+480+mgi+xy_array[1])*args.scale)
+                    cv2.circle(image, (cx, cy), int(radius*args.scale), red, thickness=2)
+
+                    cx = int((mgh+xy_array[2])*args.scale)
+                    cy = int((mgv+xy_array[3])*args.scale)
+                    cv2.circle(image, (cx, cy), int(radius*args.scale), red, thickness=2)
+
+                    cx = int((mgh+mgi+640+xy_array[4])*args.scale)
+                    cy = int((mgv+xy_array[5])*args.scale)
+                    cv2.circle(image, (cx, cy), int(radius*args.scale), red, thickness=2)
+
+                    cv2.imshow(window_name, image)
+                    cv2.waitKey(1)
+
+def receive_xy(cam_id):
+    
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the receiver's IP and port
+    sock.bind((IP_NUC, 4330+cam_id))
+
+    while True:
+        data, _ = sock.recvfrom(1024)
+        decoded_data = data.decode()
+        x, y = map(int, decoded_data.split(','))
+        # print(f"receiving {x},{y}")
+        xy_array[2*(cam_id-1)] = x
+        xy_array[2*(cam_id-1)+1] = 480-y
+
+def print_xy():
+
+    while(True):
+        message = "xy_array: "
+        for i in range(6):
+            message += f" {xy_array[i]} " 
+        print(message)
+        time.sleep(1)
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Cobotics Workspace Visualizer and Streamer')
+    
+    parser.add_argument('-c1', '--c1nbr', type=int, help="Camera #1 : lsusb number", default=8)
+    parser.add_argument('-c2', '--c2nbr', type=int, help="Camera #2 : lsusb number", default=7)
+    parser.add_argument('-c3', '--c3nbr', type=int, help="Camera #3 : lsusb number", default=9)
+    
+    parser.add_argument('-p1', '--port1', type= int, help="Port for events Cam #1", default=3331)
+    parser.add_argument('-p2', '--port2', type= int, help="Port for events Cam #2", default=3332)
+    parser.add_argument('-p3', '--port3', type= int, help="Port for events Cam #3", default=3333)
+
+    parser.add_argument('-s', '--scale', type= float, help="Image scale", default=1.8)
+    parser.add_argument('-u', '--undistortion', action='store_true', help="Enable undistortion")
+    parser.add_argument('-m', '--mode', type=str, help="Mode: live|recording", default="live")
+
+    return parser.parse_args()
+        
+
+if __name__ == '__main__':
+
+
+    args = parse_args()
+    xy_array = multiprocessing.Array('i', [0,0,0,0,0,0])
+   
+    
+    # Create an object of the cam_info class
+    cam_1 = cam_info(id=1,bus=4, nbr=args.c1nbr, p_sleipner=2301, p_nuc=3331)
+    cam_2 = cam_info(id=2,bus=4, nbr=args.c2nbr, p_sleipner=2302, p_nuc=3332)
+    cam_3 = cam_info(id=3,bus=4, nbr=args.c3nbr, p_sleipner=2300, p_nuc=3333)
+
+    # Create three parallel processes
+    p_streaming_1 = multiprocessing.Process(target=start_streaming, args=(cam_1,args.mode,args.undistortion,))
+    p_streaming_2 = multiprocessing.Process(target=start_streaming, args=(cam_2,args.mode,args.undistortion,))
+    p_streaming_3 = multiprocessing.Process(target=start_streaming, args=(cam_3,args.mode,args.undistortion,))
+
+    # Create three parallel processes
+    p_visualization = multiprocessing.Process(target=visualize_data, args=(args,))
+    p_get_xy_cam_1 = multiprocessing.Process(target=receive_xy, args=(1,))
+    p_get_xy_cam_2 = multiprocessing.Process(target=receive_xy, args=(2,))
+    p_get_xy_cam_3 = multiprocessing.Process(target=receive_xy, args=(3,))
+    # p_print_xy = multiprocessing.Process(target=print_xy)
+
+    # Start the processes
+    p_visualization.start()
+    p_streaming_1.start()
+    p_streaming_2.start()
+    p_streaming_3.start()
+    p_get_xy_cam_1.start()
+    p_get_xy_cam_2.start()
+    p_get_xy_cam_3.start()
+    # p_print_xy.start()
+
+    # Wait for the processes to finish (you may need to manually stop them)
+    p_visualization.join()
+    p_streaming_1.join()
+    p_streaming_2.join()
+    p_streaming_3.join()
+    p_get_xy_cam_1.join()
+    p_get_xy_cam_2.join()
+    p_get_xy_cam_3.join()
+    # p_print_xy.join()
---
+++ visualizers/plotter.py
@@ -0,0 +1,162 @@
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+matplotlib.use("TkAgg")
+matplotlib.rcParams['toolbar'] = 'None' 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import socket
+import struct
+import multiprocessing
+import math
+import datetime
+
+IP_NUC = '172.16.222.46'
+
+class xyz_estimate:
+    def __init__(self, queue):
+        self.q = queue
+        self.x = []
+        self.y = []
+        self.z = []
+        self.xyz = [-100,-100,-100]
+
+##############################################################################################################################
+#                                                          VISUALIZE                                                         #
+##############################################################################################################################  
+
+# This function is called periodically from FuncAnimation
+def rt_xyz(i, xyz_ncs, xyz_opt, t, axs):
+
+
+    while not xyz_ncs.q.empty():
+        xyz_ncs.xyz = xyz_ncs.q.get(False)
+
+    while not xyz_opt.q.empty():
+        xyz_opt.xyz = xyz_opt.q.get(False)
+
+    e_x = 1000*abs(xyz_ncs.xyz[0]-xyz_opt.xyz[0])
+    e_y = 1000*abs(xyz_ncs.xyz[1]-xyz_opt.xyz[1])
+    e_z = 1000*abs(xyz_ncs.xyz[2]-xyz_opt.xyz[2])
+
+    # Add x and y to lists
+    t.append(datetime.datetime.now().strftime('%H:%M:%S.%f'))
+
+    xyz_ncs.x.append(xyz_ncs.xyz[0])
+    xyz_ncs.y.append(xyz_ncs.xyz[1])
+    xyz_ncs.z.append(xyz_ncs.xyz[2])
+
+    xyz_opt.x.append(xyz_opt.xyz[0])
+    xyz_opt.y.append(xyz_opt.xyz[1])
+    xyz_opt.z.append(xyz_opt.xyz[2])
+
+    # Limit x and y lists to 100 items
+    t = t[-100:]
+
+    xyz_ncs.x = xyz_ncs.x[-100:]
+    xyz_ncs.y = xyz_ncs.y[-100:]
+    xyz_ncs.z = xyz_ncs.z[-100:]
+
+    xyz_opt.x = xyz_opt.x[-100:]
+    xyz_opt.y = xyz_opt.y[-100:]
+    xyz_opt.z = xyz_opt.z[-100:]
+
+    txt_x = txt_y = txt_z = "No signal"
+    if xyz_ncs.x[-1] != -100:
+        txt_x = "x = {:.3f} [m] (±{:.3f} [mm])".format(xyz_ncs.x[-1], e_x) 
+    if xyz_ncs.y[-1] != -100:
+        txt_y = "y = {:.3f} [m] (±{:.3f} [mm])".format(xyz_ncs.y[-1], e_y) 
+    if xyz_ncs.z[-1] != -100:
+        txt_z = "z = {:.3f} [m] (±{:.3f} [mm])".format(xyz_ncs.z[-1], e_z) 
+
+    # Draw x and y lists
+    axs[0].clear()
+    axs[0].plot(t, xyz_ncs.x, color='r')
+    axs[0].plot(t, xyz_opt.x, color='r', linestyle='--')
+    axs[0].text(t[0], 0.1, txt_x, fontsize='xx-large')
+    axs[0].xaxis.set_visible(False)
+    axs[0].set_ylim([-0.7,0.3])
+    axs[0].set_ylabel('x')
+
+    axs[1].clear()
+    axs[1].plot(t, xyz_ncs.y, color='g')
+    axs[1].plot(t, xyz_opt.y, color='g', linestyle='--')
+    axs[1].text(t[0], 0.8, txt_y, fontsize='xx-large')
+    axs[1].xaxis.set_visible(False)
+    axs[1].set_ylim([0,1])
+    axs[1].set_ylabel('y')
+
+    axs[2].clear()
+    axs[2].plot(t, xyz_ncs.z, color='b')
+    axs[2].plot(t, xyz_opt.z, color='b', linestyle='--')
+    axs[2].text(t[0], 1.3, txt_z, fontsize='xx-large')
+    axs[2].xaxis.set_visible(False)
+    axs[2].set_ylim([0.5,1.5])
+    axs[2].set_ylabel('z')
+
+    axs[0].set_title("Object Position in Workspace", fontsize='xx-large')
+
+
+
+def oscilloscope(xyz_ncs_queue, xyz_opt_queue):
+
+    # Create figure for plotting
+    fig, axs = plt.subplots(3, figsize=(8.72, 6.18))
+    fig.canvas.manager.set_window_title('World Space')
+
+
+    i = 0
+
+    t = []
+    xyz_ncs = xyz_estimate(xyz_ncs_queue)
+    xyz_opt = xyz_estimate(xyz_opt_queue)
+
+    # Set up plot to call rt_xyz() function periodically
+    ani = animation.FuncAnimation(fig, rt_xyz, fargs=(xyz_ncs, xyz_opt, t, axs), interval=1)
+    plt.show()
+
+
+
+def get_xyz(xyz_ncs_queue, port_xyz_ncs):
+
+    # Receiver address and port
+    receiver_address = (IP_NUC, port_xyz_ncs)
+
+    # Create a UDP socket
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receiver_socket.bind(receiver_address)
+
+    while True:
+        # Receive data from the sender
+        data, addr = receiver_socket.recvfrom(12)  # Assuming three floats (3 * 4 bytes each)
+
+        # Unpack the received data as three float values
+        values = struct.unpack('fff', data)
+        xyz_ncs_queue.put([values[0], values[1], values[2]])
+
+
+    # Close the socket
+    receiver_socket.close()
+
+
+if __name__ == "__main__":
+    
+
+    port_xyz_ncs = 6000
+    port_xyz_opt = 5999
+
+    xyz_ncs_queue = multiprocessing.Queue()
+    xyz_opt_queue = multiprocessing.Queue()
+
+    rt_plot = multiprocessing.Process(target=oscilloscope, args=(xyz_ncs_queue, xyz_opt_queue,))
+    xyz_ncs_receiver = multiprocessing.Process(target=get_xyz, args=(xyz_ncs_queue, port_xyz_ncs,))
+    xyz_opt_receiver = multiprocessing.Process(target=get_xyz, args=(xyz_opt_queue, port_xyz_opt,))
+
+    rt_plot.start()
+    xyz_ncs_receiver.start()
+    xyz_opt_receiver.start()
+
+    rt_plot.join()
+    xyz_ncs_receiver.join()
+    xyz_opt_receiver.join()
